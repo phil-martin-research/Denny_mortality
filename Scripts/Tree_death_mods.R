1e04c8a451892dfd04f0f8b9fce7bb3bdd3ce4b2
Phil Martin <philip.martin.1981@gmail.com>
2015-02-25 15:39:11 +0100

    modified models of mortality

--- Scripts/Tree_death_mods.R
+++ Scripts/Tree_death_mods.R
@@ -11,94 +11,92 @@ library(GGally)
 library(lme4)
 library(lattice)
 library(MuMIn)
+library(ResourceSelection)
+
 ggpairs(Dead[, 7:13])
 Dead_F<-subset(Dead,Species=="F")
 Dead_Q<-subset(Dead,Species=="Q")
+Dead_I<-subset(Dead,Species=="I")
+
+summary(Dead)
 
 ggpairs(Dead_Q[, 7:13])
 
 head(Dead_F)
 
 #candidates for growth rate variables
-ggplot(Dead_F,aes(x=GR,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_wrap(~SL)
-ggplot(Dead_F,aes(x=BAGR,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_wrap(~SL)
-ggplot(Dead_F,aes(x=relGR,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_wrap(~SL)
-ggplot(Dead_F,aes(x=relBAGR,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_wrap(~SL)
+ggplot(Dead,aes(x=GR,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_grid(Species~Year)
+ggplot(Dead,aes(x=BAGR,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_grid(Species~Year)
+ggplot(Dead,aes(x=relGR,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_grid(Species~Year)
+ggplot(Dead,aes(x=relBAGR,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_grid(Species~Year)
+
+#first look at death rate for beech for 1988, 1996, 2014
+Dead_F_88<-subset(Dead_F,Year==1988)
+Dead_F_96<-subset(Dead_F,Year==1996)
+Dead_F_14<-subset(Dead_F,Year==2014)
+
+#now build glms of these
+M0<-glm(Dead~1,Dead_F_88,family="binomial")
+M_GR<-glm(Dead~GR,Dead_F_88,family="binomial")
+M_BAGR<-glm(Dead~BAGR,Dead_F_88,family="binomial")
+M_relGR<-glm(Dead~relGR,Dead_F_88,family="binomial")
+M_relBAGR<-glm(Dead~relBAGR,Dead_F_88,family="binomial")
+
+AICc(M0,M_GR,M_BAGR,M_relGR,M_relBAGR)
+
+plot(Dead_F_88$relGR,plogis(predict(M_relGR)))
+#relative growth rate looks best
+
+#now size
+M0<-glm(Dead~1,Dead_F_88,family="binomial")
+M_DBH<-glm(Dead~DBH2,Dead_F_88,family="binomial")
+M_DBH2<-glm(Dead~DBH2+I(DBH2^2),Dead_F_88,family="binomial")
+M_BA<-glm(Dead~BA2,Dead_F_88,family="binomial")
+
+AICc(M0,M_DBH,M_DBH2,M_BA)
+plot(Dead_F_88$BA2,plogis(predict(M_BA)))
+
+#now a model of both BA and relate growth rate
+M1<-glm(Dead~BA2,Dead_F_88,family="binomial")
+M2<-glm(Dead~relGR,Dead_F_88,family="binomial")
+M3<-glm(Dead~BA2+relGR,Dead_F_88,family="binomial")
+M3<-glm(Dead~BA2+relGR,Dead_F_88,family="binomial")
+M4<-glm(Dead~BA2+I(BA^2),Dead_F_88,family="binomial")
+
+
+hl <- hoslem.test(Dead_F_88$Dead, fitted(M1), g=10)
+hl
 
 
 
-#look at different models of mortality related to growth
-#first a null model
-glmerControl(tolPwrss=1e-3)
-M0<-glmer(Dead~1+(1|ID2/SL),data=Dead_F,family=binomial)
-M_GR<-glmer(Dead~GR+(1|ID2/SL),data=Dead_F,family=binomial)
-M_BAGR<-glmer(Dead~BAGR+(1|ID2/SL),data=Dead_F,family=binomial)
-M_relGR<-glmer(Dead~relGR+(1|ID2/SL),data=Dead_F,family=binomial)
-M_relBAGR<-glmer(Dead~relBAGR+(1|ID2/SL),data=Dead_F,family=binomial)
-
-AICc(M_GR,M_BAGR,M_relGR,M_relBAGR)
-#seems like DBH growth rate is best
-
-#candidates for size
-ggplot(Dead_F,aes(x=DBH2,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_wrap(~SL)
-ggplot(Dead_F,aes(x=BA2,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_wrap(~SL)
-ggplot(Dead_F,aes(x=relSize,y=Dead))+geom_point()+geom_smooth(method=glm,family="binomial")+facet_wrap(~SL)
-
-#look at different models of mortality related to size
-#first a null model
-glmerControl(tolPwrss=1e-3)
-M0<-glmer(Dead~1+(1|ID2/SL),data=Dead_F,family=binomial)
-M_DBH<-glmer(Dead~DBH+(1|ID2/SL),data=Dead_F,family=binomial)
-M_DBH2<-glmer(Dead~DBH+I(DBH^2)+(1|ID2/SL),data=Dead_F,family=binomial)
-M_BA<-glmer(Dead~BA+(1|ID2/SL),data=Dead_F,family=binomial)
-M_BA2<-glmer(Dead~BA+I(BA^2)+(1|ID2/SL),data=Dead_F,family=binomial)
-M_relSize<-glmer(Dead~relSize+(1|ID2/SL),data=Dead_F,family=binomial)
-
-AICc(M_DBH,M_DBH2,M_BA,M_BA2,M_relSize)
-#looks like BA is best for this
-
-head(Dead_F)
 
 
-#produce different potential models
 
-str(Dead_F$SL)
-
-
-M0<-lmer(logDeath~1+(1|ID2),data=Dead_F)
-M1<-lmer(logDeath~DBH+(1|ID2),data=Dead_F)
-M2<-lmer(logDeath~GR+(1|ID2),data=Dead_F)
-M3<-lmer(logDeath~BA+GR+(1|ID2),data=Dead_F)
-M4<-lmer(logDeath~BA+I(BA^2)+GR+(1|ID2),data=Dead_F)
-
-plogis(predict(M4,re.form=NA))
-
-library(car)
-
-Dead_F$logDeath<-logit((Dead_F$Dead/Dead_F$SL))
-
-summary(M3)
-
-dotplot(ranef(M3,condVar=TRUE),
-        lattice.options=list(layout=c(1,2)))
-
-summary(M3)
+#create some predictions
+predict(M_BA,se.fit = T)$
 
 
-Dead_1988<-subset(Dead_F,Year==1988)
+limits<-as.vector(quantile(Dead_F_88$DBH2, prob = seq(0, 1, length = 11), type = 5))
 
 
-GLM1<-glm(Dead~BA2+GR,data=Dead_1988,family=binomial)
-GLM2<-glm(Dead~BA2+I(BA2^2)+GR,data=Dead_1988,family=binomial)
-GLM3<-glm(Dead~BA2+I(BA2^2),data=Dead_1988,family=binomial)
-GLM4<-glm(Dead~BA2,data=Dead_1988,family=binomial)
+Dead_F_88$BA_class<-NA
+Dead_F_882<-NULL
+for (i in 2:length(limits)){
+  Dead_sub<-subset(Dead_F_88,BA2>=limits[i-1]&BA2<limits[i])
+  Dead_sub$BA_class<-limits[i-1]
+  Dead_F_882<-rbind(Dead_sub,Dead_F_882)
+}
 
-summary(GLM1)
-summary(GLM2)
-summary(GLM3)
-summary(GLM4)
+head(Dead_F_882)
 
 
-AICc(GLM1,GLM2,GLM3,GLM4)
+BA_class<-ddply(Dead_F_882,.(BA_class),summarize,Death=mean(Dead))
+BA_class$BA2<-BA_class$BA_class
 
-plot(Dead_1988$DBH2,plogis(predict(GLM4)))
+BA_class$Pred<-plogis(predict(M1,newdata=BA_class))
+
+plot(M1)
+
+ggplot(BA_class,aes(x=as.factor(BA_class),y=Death))+geom_point()+geom_point(aes(y=Pred),colour="red")
+
+
