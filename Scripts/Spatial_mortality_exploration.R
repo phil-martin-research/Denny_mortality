4174dddfcdbcd6a2f8c2b86ceee7d091996dcc72
Phil Martin <philip.martin.1981@gmail.com>
2015-03-10 12:48:50 +0100

    added code and figures to produce correlograms to identify spatial auctocorrelation of dead trees

--- Scripts/Spatial_mortality_exploration.R
+++ Scripts/Spatial_mortality_exploration.R
@@ -4,42 +4,80 @@
 #load packages
 library(ggplot2)
 library(ncf)
+library(plyr)
+library(reshape)
+
 
 #import data
 rm(list=ls(all=TRUE))
-Mort<-read.csv("Data/Dead_size.csv")
+Mort<-read.csv("Data/Dead_spat.csv")
 Mort<-unique(Mort)
 head(Mort)
-keeps<-c("ID2","Year","Northing","Easting","Dead_cum2","Species")
-Mort_F<-subset(Mort,Species=="F")
-head(Mort_F)
+keeps<-c("ID2","Year","Northing","Easting","Dead_cum2","Species","DBH2","DBH","Dead")
+Mort<-Mort[keeps]
+Mort<-ddply(Mort, .(ID2,Year), head, n = 1) 
+
+head(Mort,n = 10)
+
+#set DBH2 to be (i) that from previous DBH reading, (ii) last DBH reading when tree was alive or (iii) current DBH for live trees
+Mort_RC<-NULL
+Tree_ID<-unique(Mort$ID2)
+for (i in 1:length(Tree_ID)){
+  Mort_sub<-subset(Mort,ID2==Tree_ID[i])
+  for (j in 1:nrow(Mort_sub)){
+    if ((Mort_sub$Year[j]==1964)==T){
+      Mort_sub$DBH2[j]<-Mort_sub$DBH[j]
+      Mort_sub$Dead_cum2[j]<-Mort_sub$Dead[j]
+    }else if (Mort_sub$Dead_cum[j]==1&(!is.na(Mort_sub$DBH[j-1]))==T){
+      Mort_sub$DBH2[j]<-Mort_sub$DBH[j-1]
+    }else if (Mort_sub$Dead_cum[j]==1&(is.na(Mort_sub$DBH[j-1]))==T){
+      Mort_sub$DBH2[j]<-Mort_sub$DBH2[j-1]
+    } else if (!is.na(Mort_sub$DBH[j])==T){
+      Mort_sub$DBH2[j]<-Mort_sub$DBH[j]
+    }else{
+      Mort_sub$DBH2[j]<-NA
+    }
+  }
+  Mort_RC<-rbind(Mort_RC,Mort_sub)
+}
+
+head(Mort_RC,n=10)
+Mort_RC$DBH10<-ifelse(Mort_RC$DBH2>10,">10cm",NA)
+Mort_RC$DBH10<-ifelse(Mort_RC$DBH2<=10,"<10cm",Mort_RC$DBH10)
+
+
+#now use just beech
+Mort_F<-subset(Mort_RC,Species=="F"&!is.na(DBH2))
+
+uyear<-length(unique(Mort_RC$Year))
 
 theme_set(theme_bw(base_size=12))
-ggplot(Mort_F,aes(x=Easting,y=Northing,colour=as.factor(Dead_cum2)))+geom_point(shape=1,size=0.5,alpha=0.5)+facet_wrap(~Year)+scale_colour_manual(values = c("green","red"))+ theme(legend.position="none")+coord_fixed()+ 
+ggplot(Mort_F,aes(x=Easting,y=Northing,colour=as.factor(Dead_cum2)))+geom_point(shape=1,size=0.5,alpha=0.5)+facet_wrap(DBH10~Year,ncol=2*uyear)+scale_colour_manual(values = c("green","red"))+ theme(legend.position="none")+coord_fixed()+ 
   theme(axis.text.x=element_blank(),
         axis.text.y=element_blank(),axis.ticks=element_blank(),
         axis.title.x=element_blank(),
         axis.title.y=element_blank())+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
-ggsave("Figures/Spat_mortality.png",width = 6,height=6,units = "in",dpi=300)
+ggsave("Figures/Spat_mortality.png",width = 12,height=6,units = "in",dpi=300)
 
-
-Beech_mort<-subset(Mort,Species=="F")
 
 #produce a correlogram of death for each year
 
 #loop to create correlograms for each year
-Years<-unique(Beech_mort$Year)
+Y_DBH<-unique(Mort_F[c("Year", "DBH10")])
 Beech_corr<-NULL
-for (i in 1:length(Years)){
-  Beech_sub<-subset(Beech_mort,Year=Years[i])
-  b.cor <- correlog(Beech_sub$Easting, Beech_sub$Northing, Beech_sub$Dead_cum,increment=1, resamp=500)
-  b.cor$P2<-ifelse(b.cor$p<0.05,"<0.05",">0.05")
-  b.cor2<-data.frame(Dist=b.cor$mean.of.class,Cor=b.cor$correlation,P_val=b.cor$P2,n=b.cor$n,Year=Years[i])
+for (i in 1:nrow(Y_DBH)){
+  Beech_sub<-subset(Mort_F,Year==Y_DBH$Year[i]&DBH10==Y_DBH$DBH10[i]&!is.na(Dead_cum2))
+  b.cor<-spline.correlog(Beech_sub$Easting, Beech_sub$Northing, Beech_sub$Dead_cum2)
+  b.cor2<-data.frame(Dist=b.cor$boot$boot.summary$predicted$x,Cor=b.cor$boot$boot.summary$predicted$y[6,],UCI=b.cor$boot$boot.summary$predicted$y[2,],LCI=b.cor$boot$boot.summary$predicted$y[10,],Year=Y_DBH$Year[i],Size=Y_DBH$DBH10[i])
   Beech_corr<-rbind(b.cor2,Beech_corr)
 }
 
-head(Beech_corr)
+
+Beech_corr2<-melt(Beech_corr,id.vars =c("Cor","UCI","LCI","Year","Size"))
+
+head(Beech_corr2)
 theme_set(theme_bw(base_size=12))
-Moran_plot1<-ggplot(Beech_corr,aes(x=Dist,y=Cor,colour=P_val,size=n))+geom_point(shape=1)+geom_line(size=1,colour="black",alpha=0.2)+facet_wrap(~Year)+xlim(0,250)
-Moran_plot2<-Moran_plot1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))+ theme(legend.position="none")
-Moran_plot2
+Moran_plot1<-ggplot(Beech_corr2,aes(x=value,y=Cor,ymax=LCI,ymin=UCI))+geom_ribbon(alpha=0.5)+geom_line(size=1,colour="black")+facet_grid(Size~Year)
+Moran_plot2<-Moran_plot1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))+ theme(legend.position="none")+xlim(0,50)+ylim(-0.5,1)
+Moran_plot2+geom_hline(y=0,lty=2)+xlab("Distance between trees (m)")+ylab("Moran's I correlation")+scale_size_continuous(range = c(1,3))
+ggsave("Figures/Dead_correl.png",height=6,width=8,units="in",dpi=300)
