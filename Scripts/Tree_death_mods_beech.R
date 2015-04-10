3c21a95cb2b13bfc98d8762a636af6838220dbfd
Phil Martin <philip.martin.1981@gmail.com>
2015-04-10 12:12:08 +0200

    modified script to remove calculation of goodness of fit tests

--- Scripts/Tree_death_mods_beech.R
+++ Scripts/Tree_death_mods_beech.R
@@ -27,7 +27,6 @@ hist(Dead$relBAGR)
 
 #subset data into individual species
 Dead_F<-subset(Dead,Species=="F")
-Dead_F<-subset(Dead_F,GR>-20&GR<20&relBAGR<4)
 keeps<-c("Dead","Easting","Northing","SL","GR","BAGR","relGR","relBAGR","DBH2","BA2","relSize","Dead_dist","Dead_No")
 Dead_F<-Dead_F[keeps]
 str(Dead_F)
@@ -58,8 +57,8 @@ M_BA<-glmer(Dead~BA2+(BA2|SL),Dead_F_st,family="binomial")
 M_BA2<-glmer(Dead~BA2+I(BA2^2)+(BA2|SL),Dead_F_st,family="binomial")
 
 AICc(M0,M_DBH,M_DBH2,M_BA,M_BA2)
-plot(Dead_F_st$DBH2,plogis(predict(M_DBH2,re.form=NA)))
-points(Dead_F_st$DBH2,plogis(predict(M_DBH2)),col="red")
+plot(Dead_F_st$BA2,plogis(predict(M_BA2,re.form=NA)))
+points(Dead_F_st$BA2,plogis(predict(M_BA2)),col="red")
 
 #bigger trees are more likley to die - DBH is the best predictor of this
 
@@ -75,32 +74,37 @@ points(Dead_F_st$Dead_dist,plogis(predict(M_D_dist)),col="red")
 
 #so BA of dead trees seems best
 #remove variables that I'm not going to use in the analyses
-Dead_F_st$DBH2_sq<-Dead_F_st$DBH2^2
-keeps<-c("Dead","DBH2","DBH2_sq","GR","Dead_dist","SL")
+Dead_F_st$BA2_sq<-Dead_F_st$BA2^2
+head(Dead_F_st)
+keeps<-c("Dead","BA2","BA2_sq","GR","Dead_dist","SL")
 Dead_F_st2<-Dead_F_st[keeps]
 
 
 ggpairs(Dead_F_st2[,2:5])
 
 #now a model of DBH, growth, distance to dead trees, number of live trees
-M0.1<-glmer(Dead~1+(DBH2|SL)+(GR|SL)+(Dead_dist|SL),Dead_F_st2,family="binomial")
-M0.2<-glmer(Dead~1+(DBH2|SL)+(GR|SL),Dead_F_st2,family="binomial")
-M0.3<-glmer(Dead~1+(DBH2|SL),Dead_F_st2,family="binomial")
+M0.1<-glmer(Dead~1+(BA2|SL)+(GR|SL)+(Dead_dist|SL),Dead_F_st2,family="binomial")
+M0.2<-glmer(Dead~1+(BA2|SL)+(GR|SL),Dead_F_st2,family="binomial")
+M0.3<-glmer(Dead~1+(BA2|SL),Dead_F_st2,family="binomial")
 M0.4<-glmer(Dead~1+(GR|SL),Dead_F_st2,family="binomial")
 AICc(M0.1,M0.2,M0.3,M0.4)
 
 
 #now put in fixed effects
 Dead_F_st2$SL2<-as.factor(Dead_F_st2$SL)
-M1<-glmer(Dead~GR+DBH2_sq+DBH2+Dead_dist+(GR|SL2),data=Dead_F_st2,family="binomial")
 
-summary(M1)
+M1.1<-glmer(Dead~GR+BA2_sq+BA2+Dead_dist+(GR|SL2)+(BA2|SL2),data=Dead_F_st2,family="binomial")
+M1.2<-glmer(Dead~GR+BA2_sq+BA2+Dead_dist+(BA2|SL2),data=Dead_F_st2,family="binomial")
+M1.3<-glmer(Dead~GR+BA2_sq+BA2+Dead_dist+(GR|SL2),data=Dead_F_st2,family="binomial")
+M1.4<-glmer(Dead~GR+BA2_sq+BA2+Dead_dist+(1|SL2),data=Dead_F_st2,family="binomial")
+AICc(M1.1,M1.2,M1.3,M1.4)
+
 dotplot(ranef(M1,condVar=TRUE),
         lattice.options=list(layout=c(1,2)))
 
 #now do model averaging
 options(na.action = "na.fail")
-models<-dredge(M1,trace = T,subset=dc(DBH2,DBH2_sq))
+models<-dredge(M1.3,trace = T,subset=dc(DBH2,DBH2_sq))
 
 #produce model selection table
 MS<-model.sel(models)
@@ -109,31 +113,17 @@ importance(MS)
 
 Avs<-model.avg(MS,fit = T,subset =cumsum(weight) <= 0.95)
 summary(Avs)
-#produce le Cessie-van Houwelingen normal test statistic for these models 
-M1_sel<-glmer(Dead~DBH2+GR+(DBH2|SL)+(GR|SL),Dead_F_st2,family="binomial")
-HLgof.test(fit = fitted(M1_sel), obs=Dead_F_st2$Dead,X=model.matrix(Dead ~ DBH2+GR,data=Dead_F_st2))
+importance(Avs)
 
-#calculate ROC
-fit<-fitted(M1_sel)
-fit.pos <- fit[Dead_F_st2$Dead==1]
-length(fit.pos)
-fit.neg <- fit[Dead_F_st2$Dead==0]
-wilcox.test(x=fit.pos, y=fit.neg)
-283362/(length(fit.pos)*length(fit.neg))
+#produce predictions from the model averaged coefficients
+summary(Dead_F_st2)
 
+new.data.BA<-data.frame(BA2=seq(-0.6578,7,0.01),GR=mean(Dead_F_st2$GR),Dead_dist=mean(Dead_F_st2$Dead_dist))
+new.data.BA$BA2_sq<-new.data.BA$BA2^2
+new.data.BA$Dead<-plogis(predict(Avs,newdata =new.data.BA,re.form=NA))
 
-
-#produce le Cessie-van Houwelingen normal test statistic for these models 
-HLgof.test(fit = fitted(M1_sel), obs=Dead_F_st2$Dead,X=model.matrix(Dead ~ DBH2+GR,data=Dead_F_st2))
-
-
-Preds<-predict(Avs)
-
-#look at partial residuals for this
-DBH_res<-(Dead_F_st2$Dead-plogis(Preds))/
-          ((plogis(Preds))*(1-plogis(Preds)))+
-          Dead_F_st2$DBH2*coef(Avs)[2]
-
+plot(Dead_F$BA2,Dead_F$Dead)
+points((new.data.BA$BA2*sd(Dead_F$BA2))+mean(Dead_F$BA2),new.data.BA$Dead,col="red")
 
 
 
