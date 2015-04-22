1c6f46cdf223d81cb0b08c889a72c828a530493f
Phil Martin <philip.martin.1981@gmail.com>
2015-04-22 11:15:40 +0200

    added table to give details fo seeling density in Denny

--- Scripts/Seedling_density.R
+++ Scripts/Seedling_density.R
@@ -2,27 +2,62 @@
 #in Denny plots and how this relates to canopy/basal area/mature density
 
 #author: Phil Martin
-#last edited: 27/03/15
-
-#load data
-rm(list=ls(all=TRUE))
-Seed<-read.csv("Data/GF_ab.csv")
-Plots<-read.csv("Data/Denny_plots_trees_M.csv")
+#last edited: 22/04/15
 
 #load packages
 library(ggplot2)
 library(lme4)
+library(MuMIn)
+library(plyr)
+library(reshape2)
+library(tidyr)
+library(dplyr)
+library(GGally)
+library(plotrix)
 
-#change year for seedling to match other dataset
-Seed$Year<-ifelse(Seed$Year==2001,1996,Seed$Year)
+#load data
+rm(list=ls(all=TRUE))
+Seedlings<-read.csv("Data/Seedlings_denny.csv",stringsAsFactors=F)
+Plots<-read.csv("Data/Denny_plots_trees_M.csv")
+Browsing<-read.csv("Data/Browsing_Denny.csv",,stringsAsFactors=F)
 
-#merge the two datsets
-Seed_plot<-merge(Seed,Plots,by=c("Block","Year"))
-Seed_plot[is.na(Seed_plot)] <- 0 #replace NAs with zeros
+#tidy seedlings data
+Seedlings$Plot<-sapply(strsplit(Seedlings$Plot2,"-"),'[',2)
+drop<-"Plot2"
+Seedlings<-Seedlings[,!(names(Seedlings) %in% drop)]
+head(Seedlings)
+Seedlings2<-subset(Seedlings,Species=="Fagus sylvatica"|Species=="Quercus spp"|Species=="Ilex aquifolium")
+Seed_melt<-melt(Seedlings2,id.vars = c("Plot","Count"))
+Seed_melt2<-ddply(Seed_melt,.(Plot,value),summarize,Count=sum(Count)*100)
+Seed_melt3<-spread(Seed_melt2, value, Count)
+Seed_melt3[is.na(Seed_melt3)] <- 0
+colnames(Seed_melt3)<-c("Plot","Beech","Holly","Oak")
 
-#exploratory plots
-ggplot(Seed_plot,aes(x=F_BA,y=Beech))+geom_point()+facet_wrap(~Year)
-ggplot(Seed_plot,aes(x=I_BA,y=Holly))+geom_point()+facet_wrap(~Year)
-ggplot(Seed_plot,aes(x=Q_BA,y=Oak))+geom_point()+facet_wrap(~Year)
+#tidy browsing data
+Browsing$Plot2<-sapply(strsplit(Browsing$Plot,"-"),'[',2)
+drop<-"Plot"
+Browsing<-Browsing[,!(names(Browsing) %in% drop)]
+colnames(Browsing)[7]<-"Plot"
+Browsing<-Browsing[complete.cases(Browsing),]
 
-#currently there is not enough data to do anything with here - need data from this years survey on abundance
+#merge browsing data and seedling data
+Seed_browsing<-merge(Browsing,Seed_melt3,by="Plot")
+str(Seed_browsing)
+
+#explore the relationships between seedling density and
+#browsing pressure/canopy openness
+ggpairs(Seed_browsing[2:10])
+
+#explore in more detail
+#canopy relationship with beech seedlings
+ggplot(Seed_browsing,aes(x=Canopy_open,y=Beech))+geom_point()+geom_smooth(method="glm",family="poisson")
+#canopy openness and holly
+ggplot(Seed_browsing,aes(x=Canopy_open,y=Holly))+geom_point()+geom_smooth(method="glm",family="poisson")
+
+
+#before analysis produce mean +/- SE for each species
+#and put in a table for later
+Seedling_density<-data.frame(Species=c("Beech","Oak","Holly"),
+           Mean_seedling_density=c(mean(Seed_browsing$Beech),mean(Seed_browsing$Oak),mean(Seed_browsing$Holly)),
+           SE=c(std.error(Seed_browsing$Beech),std.error(Seed_browsing$Oak),std.error(Seed_browsing$Holly)))
+write.csv(Seedling_density,"Tables/Seedling_density.csv",row.names=F)
