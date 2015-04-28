d2aa5c412b614e58ea268a2c0dcf1309b3119316
Phil Martin <philip.martin.1981@gmail.com>
2015-04-28 15:53:29 +0200

    heavily modified data cleaning process to include data from unenclosed transect

--- Scripts/Mort_Data_cleaning.R
+++ Scripts/Mort_Data_cleaning.R
@@ -172,23 +172,39 @@ for (i in 1:length(Uni_Tree)){
 Tree_dead3<-Tree_dead3[with(Tree_dead3, order(ID2,Year)), ]
 head(Tree_dead3,n = 20)
 
-Tree_dead4<-NULL
+
 Tree_dead3$SL<-NA
-Years<-rbind(data.frame(Year=unique(Tree_dead3$Year),SL=c(NA,20,4,12,18),Transect1=1,Transect2=51),
-             data.frame(Year=c(1964,1999,2014),SL=c(NA,35,15),Transect1=51,Transect2=90))    
-for (i in 1:nrow(Years)){
-  Dead_sub<-subset(Tree_dead3,Year==Years$Year[i]&Block>=Years$Transect1[i]&Block<Years$Transect2[i])
-  Dead_sub$SL<-Years$SL[i]
+Years_1<-data.frame(Year=c(1964,1984,1988,1996,2014),SL=c(NA,20,4,12,18))
+Tree_dead4_2<-NULL
+Tree_dead3_1<-subset(Tree_dead3,Block<51)
+for (i in 1:nrow(Years_1)){
+  Dead_sub<-subset(Tree_dead3_1,Year==Years_1$Year[i])
+  Dead_sub$SL<-Years_1$SL[i]
   for (j in 1:nrow(Dead_sub)){
     BA_sub<-subset(Dead_sub,BA2<Dead_sub$BA2[j])
     BA_sum<-sum(BA_sub$BA2,na.rm = T)
     BA_sum2<-sum(Dead_sub$BA2,na.rm = T)
     Dead_sub$relSize[j]<-BA_sum/BA_sum2
   }
-  Tree_dead4<-rbind(Dead_sub,Tree_dead4)
+  Tree_dead4_1<-rbind(Dead_sub,Tree_dead4_1)
 }
 
+Years_2<-data.frame(Year=c(1964,1999,2014),SL=c(NA,35,15))
+Tree_dead3_2<-subset(Tree_dead3,Block>51)
+Tree_dead4_2<-NULL
+for (i in 1:nrow(Years_2)){
+  Dead_sub<-subset(Tree_dead3_2,Year==Years_2$Year[i])
+  Dead_sub$SL<-Years_2$SL[i]
+  for (j in 1:nrow(Dead_sub)){
+    BA_sub<-subset(Dead_sub,BA2<Dead_sub$BA2[j])
+    BA_sum<-sum(BA_sub$BA2,na.rm = T)
+    BA_sum2<-sum(Dead_sub$BA2,na.rm = T)
+    Dead_sub$relSize[j]<-BA_sum/BA_sum2
+  }
+  Tree_dead4_2<-rbind(Dead_sub,Tree_dead4_2)
+}
 
+Tree_dead4<-rbind(Tree_dead4_1,Tree_dead4_2)
 Tree_dead4<-Tree_dead4[with(Tree_dead4, order(ID2,Year)), ]
 head(Tree_dead4,n = 20)
 tail(Tree_dead4,n = 20)
@@ -200,6 +216,7 @@ keeps<-c("ID2","Easting","Northing","Species")
 DBH_Loc<-DBH_ID[keeps]
 DBH_Loc<-unique(DBH_Loc)
 DBH_Loc<-DBH_Loc[with(DBH_Loc, order(ID2)), ]
+head(Tree_dead4)
 Dead_loc<-merge(Tree_dead4,DBH_Loc,by="ID2",all.x=T)
 Dead_loc2<-subset(Dead_loc,Species=="Q"|Species=="F"|Species=="I")
 Dead_loc2$Dead2<-ifelse(is.na(Dead_loc2$Dead),0,Dead_loc2$Dead2)
@@ -226,16 +243,16 @@ library(rgeos)
 #trees
 
 SU<-unique(Dead_loc2$Species)
-Yr<-rbind(data.frame(Year=unique(Tree_dead3$Year),SL=c(NA,20,4,12,18),Transect1=1,Transect2=51),
-          data.frame(Year=c(1964,1999,2014),SL=c(NA,35,15),Transect1=51,Transect2=90))    
+Years_1<-data.frame(Year=c(1964,1984,1988,1996,2014),SL=c(NA,20,4,12,18))
+Dead_loc2_1<-subset(Dead_loc2,Block<51)
 Years<-NULL
-Years2<-NULL
+Years2_1<-NULL
 for (i in 1:length(SU)){
-  Sub_sp<-subset(Dead_loc2,Species==SU[i])
-  for (j in 2:length(Yr)){
-    if (nrow(subset(Sub_sp,Year==Yr[j-1]&Dead_cum2==1))>0){
-    Yr1<-subset(Sub_sp,Year==Yr[j-1]&Dead_cum2==1&DBH2>10)
-    Yr2<-subset(Sub_sp,Year==Yr[j])
+  Sub_sp<-subset(Dead_loc2_1,Species==SU[i])
+  for (j in 2:nrow(Years_1)){
+    if (nrow(subset(Sub_sp,Year==Years_1$Year[j-1]&Dead_cum2==1))>0){
+    Yr1<-subset(Sub_sp,Year==Years_1$Year[j-1]&Dead_cum2==1&DBH2>10)
+    Yr2<-subset(Sub_sp,Year==Years_1$Year[j])
     Yr2$Dead_dist<-NA
     a <- SpatialPointsDataFrame(coords = data.frame(x = Yr2$Easting, y =Yr2$Northing ),data=data.frame(Yr2$BA))
     b <- SpatialPointsDataFrame(coords = data.frame(x = Yr1$Easting, y =Yr1$Northing ),data=data.frame(Yr1$BA))
@@ -253,7 +270,7 @@ for (i in 1:length(SU)){
       Yr2$Dead_dist[k]<-min(results[k,1:ncol(results)],na.rm = T)
     }
     }else{
-      Yr2<-subset(Sub_sp,Year==Yr[j])
+      Yr2<-subset(Sub_sp,Year==Years_1$Year[j])
       Yr2$Dead_No<-0
       #Yr2$Dead_BA<-0
       Yr2$Dead_dist<-NA
@@ -261,11 +278,49 @@ for (i in 1:length(SU)){
     Years<-rbind(Years,Yr2)
     
   }
-  Years2<-rbind(Years,Years2)
+  Years2_1<-rbind(Years,Years2_1)
 }
 
-Years2$Dead_BA<-ifelse(is.na(Years2$Dead_BA),0,Years2$Dead_BA)
-summary(Years2)
+SU<-c("I","F")
+Years_2<-data.frame(Year=c(1964,1999,2014),SL=c(NA,35,15))
+Dead_loc2_2<-subset(Dead_loc2,Block>51)
+Years<-NULL
+Years2_2<-NULL
+for (i in 1:length(SU)){
+  Sub_sp<-subset(Dead_loc2_2,Species==SU[i])
+  for (j in 2:nrow(Years_2)){
+    if (nrow(subset(Sub_sp,Year==Years_2$Year[j-1]&Dead_cum2==1))>0){
+      Yr1<-subset(Sub_sp,Year==Years_2$Year[j-1]&Dead_cum2==1&DBH2>10)
+      Yr2<-subset(Sub_sp,Year==Years_2$Year[j])
+      Yr2$Dead_dist<-NA
+      a <- SpatialPointsDataFrame(coords = data.frame(x = Yr2$Easting, y =Yr2$Northing ),data=data.frame(Yr2$BA))
+      b <- SpatialPointsDataFrame(coords = data.frame(x = Yr1$Easting, y =Yr1$Northing ),data=data.frame(Yr1$BA))
+      plot(a)
+      points(b,col="red")
+      buffer<- gBuffer( a, width=10, byid=TRUE )
+      plot(buffer)
+      points(a,col="red",)
+      
+      Yr2$Dead_No<-as.numeric(sapply(over(buffer, geometry(b), returnList = TRUE), length))
+      #Yr2$Dead_BA<-as.numeric(over(buffer,b,fn=sum)[,1])
+      results <- spDists(a, b, longlat=F)  
+      results<-ifelse(results==0,NA,results)
+      for (k in 1:nrow(results)){    
+        Yr2$Dead_dist[k]<-min(results[k,1:ncol(results)],na.rm = T)
+      }
+    }else{
+      Yr2<-subset(Sub_sp,Year==Years_2$Year[j])
+      Yr2$Dead_No<-0
+      #Yr2$Dead_BA<-0
+      Yr2$Dead_dist<-NA
+    }
+    Years<-rbind(Years,Yr2)
+    
+  }
+  Years2_2<-rbind(Years,Years2_2)
+}
+
+Years2<-rbind(Years2_1,Years2_2)
 Years3<-unique(Years2)
 head(Years3)
 
