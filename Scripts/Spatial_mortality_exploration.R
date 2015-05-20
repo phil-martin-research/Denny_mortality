2cbb772af2bb136d00539b373f34e7eb1ae1d4d8
Phil Martin <philip.martin.1981@gmail.com>
2015-05-20 11:33:52 +0200

    modified spatial auto-correlation figures

--- Scripts/Spatial_mortality_exploration.R
+++ Scripts/Spatial_mortality_exploration.R
@@ -49,22 +49,24 @@ Mort_RC$DBH10<-ifelse(Mort_RC$DBH2<=10,"<10cm",Mort_RC$DBH10)
 #produce a correlogram of death for each year for each species
 
 #loop to create correlograms for each year
-Un_Sp_DBH<-expand.grid(Species=unique(Mort_RC$Species),DBH10=c(">10cm","<10cm"))
+Un_Sp_DBH<-expand.grid(Species=unique(Mort_RC$Species))
 for (y in 1:nrow(Un_Sp_DBH)){
-Mort_sub<-subset(Mort_RC,Species==Un_Sp_DBH$Species[y]&DBH10==Un_Sp_DBH$DBH10[y])
+Mort_sub<-subset(Mort_RC,Species==Un_Sp_DBH$Species[y])
 YU<-unique(Mort_sub$Year)
+print(Un_Sp_DBH$Species[y])
 Corr<-NULL
 for (i in 1:length(YU)){
-  Beech_sub<-subset(Mort_sub,Year==YU[1]&!is.na(Dead_cum2))
-  b.cor<-spline.correlog(Beech_sub$Easting, Beech_sub$Northing, Beech_sub$Dead_cum2,xmax = 100)
-  b.cor2<-data.frame(Dist=b.cor$boot$boot.summary$predicted$x[1,],Cor=b.cor$boot$boot.summary$predicted$y[6,],UCI=b.cor$boot$boot.summary$predicted$y[2,],LCI=b.cor$boot$boot.summary$predicted$y[10,],Year=YU[i],Size=Un_Sp_DBH$DBH10[y])
+  Beech_sub<-subset(Mort_sub,Year==YU[i]&!is.na(Dead_cum2))
+  b.cor<-spline.correlog(Beech_sub$Easting, Beech_sub$Northing, Beech_sub$Dead_cum2,xmax = 100, resamp=1000,quiet=T)
+  b.cor2<-data.frame(Dist=b.cor$boot$boot.summary$predicted$x[1,],Cor=b.cor$boot$boot.summary$predicted$y[6,],UCI=b.cor$boot$boot.summary$predicted$y[2,],LCI=b.cor$boot$boot.summary$predicted$y[10,],Year=YU[i])
   Corr<-rbind(b.cor2,Corr)
+  print(YU[i])
 }
 theme_set(theme_bw(base_size=12))
 Moran_plot1<-ggplot(Corr,aes(x=Dist,y=Cor,ymax=LCI,ymin=UCI))+geom_ribbon(alpha=0.2)+geom_line(size=0.5,colour="black")+facet_wrap(~Year)
-Moran_plot2<-Moran_plot1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))+ theme(legend.position="none")+xlim(0,50)
+Moran_plot2<-Moran_plot1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))+ theme(legend.position="none")+xlim(0,100)
 Moran_plot2+geom_hline(y=0,lty=2)+xlab("Distance between trees (m)")+ylab("Moran's I correlation")+scale_size_continuous(range = c(1,3))
-ggsave(paste("Figures/Dead_correl_",Un_Species[i],".png",sep=""),height=6,width=8,units="in",dpi=300)
+ggsave(paste("Figures/Dead_correl_",Un_Sp_DBH$Species[y],".png",sep=""),height=6,width=8,units="in",dpi=300)
 }
 
 
